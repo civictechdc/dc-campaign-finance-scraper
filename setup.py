a4afc3cbbb19d25ed49576732bbc275aa486a95d
Saul <s.shanabrook@gmail.com>
2014-07-25 21:04:18 -0400

    bumper version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dc-campaign-finance-scraper",
-    version="0.6.1",
+    version="0.6.2",
     packages=find_packages(exclude=["tests.*"]),
     entry_points={
         'console_scripts': [
