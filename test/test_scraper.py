1d93dd8d2988dd7bcc4d898a4c169192b973755a
Saul Shanabrook <s.shanabrook@gmail.com>
2014-08-18 10:19:15 -0400

    update tests for new dataset format

--- test/test_scraper.py
+++ test/test_scraper.py
@@ -1,10 +1,16 @@
 from dc_campaign_finance_scraper import scraper
-from dc_campaign_finance_scraper import utils
 
 
 class TestRecords(object):
     def test_gets_ellissa(self):
 
         records = scraper.records('01/01/2014', '01/01/2015', 'con')
-        candidates = set(map(lambda record: utils.dict_from_dataset_row(record, records)['Candidate Name'], records))
+        candidates = set(map(lambda record: record['Candidate Name'], records.dict))
+        assert "Elissa Silverman" in candidates
+
+
+class TestRecordsWithOfficeAndElectionYear(object):
+    def test_gets_ellissa(self):
+        records = scraper.records_with_office_and_election_year('01/01/2014', '01/01/2015', 'con')
+        candidates = set(map(lambda record: record['Candidate Name'], records.dict))
         assert "Elissa Silverman" in candidates
